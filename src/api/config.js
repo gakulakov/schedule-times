501b96e214ad20bcdc6607761759688501589e23
Georgy Kulakov <gakulakov.ssx@gmail.com>
2021-08-03 01:18:02 +0300

    Done

---
+++ src/api/config.js
@@ -0,0 +1,6 @@
+export const config = {
+    headers: {
+        'X-Access-Token': 'crm_b48d0351067f704c8e5344aa3bbc2ea09751fe418aff4a8a7bed9fb13a7389ba',
+        'Accept': 'application/json'
+    }
+}
